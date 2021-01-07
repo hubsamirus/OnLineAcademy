fb45cfdc5373422ad5f9e31e152548f0f3075af0
samirus <vivelequebec2012@gmail.com>
2021-01-06 19:09:43 -0500

    Remove server_side directory

--- server_side/models/user.js
+++
@@ -1,9 +0,0 @@
-const db = require('../util/database');
-
-module.exports = class User{
-    constructor(name, email, password){
-        this.name = name;
-        this.email = email;
-        this.password = password;
-    };
-}
